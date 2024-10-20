a90d299d5c7d3c0338b9f7b14ba72bbeed843af1
ANAY JOSHI <JOSHI2AN@MAIL.UC.EDU>
2024-10-20 16:23:37 -0400

    Implemented multiple changes in different files.....

--- src/App.svelte
+++ src/App.svelte
@@ -173,7 +173,7 @@
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   }
 
-  /* Light and dark mode grid item styles */
+  /* Light and dark mode grid item styles for better UI view */
   .grid-item.is-light {
     background-color: aqua;
     color: navy !important;
