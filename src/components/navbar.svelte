b43e6067425843d29f7c919f25b2942f7e1ddcfb
ANAY JOSHI <JOSHI2AN@MAIL.UC.EDU>
2024-10-21 17:22:31 -0400

    Final Refactoring of Navbar and Updating Map link

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -90,6 +90,6 @@
     <NavLi on:click={toggleScheduleModal} style="cursor: pointer; font-weight: bold; font-family:Georgia, 'Times New Roman', Times, serif; color: #ff13f0;">Schedule</NavLi>
     <NavLi on:click={toggleHistoryModal} style="cursor: pointer; font-weight: bold; font-family:Georgia, 'Times New Roman', Times, serif; color: #ff13f0;">History</NavLi>
     <NavLi on:click={toggleMapModal} style="cursor: pointer; font-weight: bold; font-family:Georgia, 'Times New Roman', Times, serif; color: #ff13f0;">Map</NavLi>
-    <NavLi on:click={toggleSupportModal} style="cursor: pointer;">Support</NavLi>
+    <NavLi on:click={toggleSupportModal} style="cursor: pointer; font-weight: bold; font-family:Georgia, 'Times New Roman', Times, serif; color: #ff13f0;">Support</NavLi>
   </NavUl>
 </FlowNavbar>
